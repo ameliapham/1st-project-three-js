458c2888214ff55c44b999601026946fb11de0b9
ameliapham <ptt.huong.8895@gmail.com>
2023-12-02 17:09:07 +0100

    Change the scale of the object

--- script.js
+++ script.js
@@ -10,14 +10,25 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({color: 'red'})
 const mesh = new THREE.Mesh(geometry, material)
-/*mesh.position.x = 1.3
-mesh.position.y = -0.5
-mesh.position.z = 1*/
-mesh.position.set(0.7,-0.6,1)
 scene.add(mesh)
 
+// Position
+//mesh.position.x = 1.3
+//mesh.position.y = -0.5
+//mesh.position.z = 1
+mesh.position.set(1,-0.9,-2)
+
+// Scale
+//mesh.scale.x = 2.5
+//mesh.scale.y = 2
+//mesh.scale.z = 0.5
+mesh.scale.set(2.5,2,0.5)
+
+
+
+
 // Axes helper
-const axesHelper = new THREE.AxesHelper()
+const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
 
 
@@ -30,8 +41,6 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.y = 1
-camera.position.x = 1
 scene.add(camera)
 
 // Renderer
