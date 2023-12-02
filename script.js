75ede48668e8e49bf470073b5ce031c57519ea92
ameliapham <ptt.huong.8895@gmail.com>
2023-12-02 19:06:33 +0100

    Add other valeur of animation

--- script.js
+++ script.js
@@ -61,10 +61,7 @@ renderer.setSize(sizes.width, sizes.height)
 const animation = () => {
     // Update Object
     cube1.rotation.x += 0.01
-    cube1.rotation.y += 0.01
-    cube2.rotation.x += 0.01
-    cube2.rotation.y += 0.01
-    cube3.rotation.x += 0.01
+    cube2.rotation.y += -0.01
     cube3.rotation.y += 0.01
 
     // Render
