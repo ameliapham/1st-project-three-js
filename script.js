a0bd653130bfccf39feb99dbd6ac7910f69f53ed
ameliapham <ptt.huong.8895@gmail.com>
2023-12-06 19:01:32 +0100

    Reduire element sizes

--- script.js
+++ script.js
@@ -38,8 +38,8 @@ const cursor = {
     y : 0
 }
 window.addEventListener('mousemove', (eventCursor) => {
-    cursor.x = eventCursor.clientX / sizes.width - 0.5
-    cursor.y = - (eventCursor.clientY / sizes.height - 0.5)
+    cursor.x = eventCursor.clientX / window.innerWidth - 0.5
+    cursor.y = - (eventCursor.clientY / window.innerHeight - 0.5)
 })*/
 
 // Axes helper
@@ -47,29 +47,29 @@ const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
 
 
-// Sizes
+/* Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
-}
+}*/
 
 window.addEventListener('resize', () => {
-    // Update sizes 
+    /* Update sizes 
     sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    sizes.height = window.innerHeight */
 
     // Update camera 
-    camera.aspect = sizes.width / sizes.height
+    camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
 
     // Update renderer 
-    renderer.setSize(sizes.width, sizes.height)
+    renderer.setSize(window.innerWidth, window.innerHeight)
 
 })
 
 // Camera
-const camera = new THREE.PerspectiveCamera(100, sizes.width / sizes.height)
-// const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight)
+// const aspectRatio = window.innerWidth / window.innerHeight
 // const camera = new THREE.OrthographicCamera(-5 * aspectRatio, 5 * aspectRatio, 3 * aspectRatio, -3 * aspectRatio, 1, 1000 )
 // camera.position.x = 1
 // camera.position.y = 1
@@ -86,7 +86,7 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.setSize(sizes.width, sizes.height)
+renderer.setSize(window.innerWidth, window.innerHeight)
 
 
 // Animation
